9e895387a5d80b30e81a96c6cb73190d3692b0bd
rumeone <yuaroslav_29@mail.ru>
2020-05-03 00:43:13 +0700

    Add new mode(second mode)

--- modes.c
+++ modes.c
@@ -68,3 +68,52 @@ int firstMode(Dictionary* d, int count)
     free(a);
     return score;
 }
+
+int secondMode(Dictionary *d, int count) {
+
+    time_t t = time(NULL);
+    struct tm* aTm = localtime(&t);
+
+    List* e_word;
+    List* r_word;
+    int* a = NULL;
+    float score = 0;
+
+    char* result = malloc(sizeof(char) * 800);
+    char* temp = malloc(sizeof(char) * 100);
+    char* str = malloc(sizeof(char) * 10);
+
+    if (result == NULL || temp == NULL || str == NULL) {
+        return -1;
+    }
+
+    result[0] = '\0';
+
+    sprintf(temp, "Второй режим, сложность %d слов.\n", count);
+    strcat(result, temp);
+    generate_rand(&a, count, d->count - 1);
+
+    for (int i = 0; i < count; i++) {
+        r_word=list_search(d->lines[a[i]], 0);
+        fprintf(stdout, "Введите перевод слова %s на английский язык:\n", r_word->key);
+        fscanf(stdin, "%s", str);
+        e_word = list_lookup(d->lines[a[i]], str);
+        if (e_word != NULL) {
+            score++;
+        }
+        sprintf(temp, "%d) Слово: %-10s Вы ввели: %-10s правильный ответ: %-10s\n", i+1, r_word->key, str, e_word->key);
+        strcat(result, temp);
+    }
+    sprintf(temp, "Ваш счет %.0f слов из %d, %.0f%%\n\n", score, count, score / count * 100);
+    strcat(result, temp);
+
+    FILE* file;
+    if ((file = fopen("results.log", "a+")) == NULL) {
+        fprintf(stdout, "Не удалось открыть файл, результаты не будут записаны.");
+    }
+    fprintf(file, "%04d-%02d-%02d\n%02d:%02d:%02d\n", aTm->tm_year + 1900, aTm->tm_mon + 1, aTm->tm_mday, aTm->tm_hour, aTm->tm_min, aTm->tm_sec);
+    fprintf(file, "%s", result);
+    fclose(file);
+    free(a);
+    return score;
+}
