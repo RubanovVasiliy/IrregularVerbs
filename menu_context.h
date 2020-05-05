288545c6db93d43dc37fb4e9a9655a80161bfec6
rumeone <yuaroslav_29@mail.ru>
2020-05-05 23:35:49 +0700

    Add menu context

---
+++ menu_context.h
@@ -0,0 +1,29 @@
+const char* menu[] = {
+    "Start testing",
+    "Instruction manual",
+    "Exit",
+};
+
+const char* menu2[] = {
+    "First level",
+    "Second level",
+    "Third level",
+    "Back",
+};
+
+const char* menu3[] = {
+    "First test mode",
+    "Second test mode",
+    "Third test mode",
+    "Back"
+};
+
+const char* instruct[] = {
+	"There will be instructions for working",
+  "with the program and other other other other",
+  "Press any key to exit",
+};
+
+const char* testin[] = {
+ "Here will be our test code"
+};
