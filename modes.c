9600961b77c9e379537c98b4739d810efee0dfe8
Vasiliy <rubanovas2005@gmail.com>
2020-05-08 23:26:28 +0700

    Adapt firstMode for ncurses

--- modes.c
+++ modes.c
@@ -2,6 +2,7 @@
 #include "dictionary.h"
 #include <assert.h>
 #include <ctype.h>
+#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,8 +21,9 @@ void generate_rand(int** a, unsigned n, unsigned range)
     assert(temp == n);
 }
 
-int firstMode(Dictionary* d, int count)
+int firstMode(WINDOW* win, Dictionary* d, int count)
 {
+    echo();
     time_t t = time(NULL);
     struct tm* aTm = localtime(&t);
 
@@ -29,9 +31,9 @@ int firstMode(Dictionary* d, int count)
     int* a = NULL;
     float score = 0;
 
-    char* result = malloc(sizeof(char) * 800);
-    char* temp = malloc(sizeof(char) * 100);
-    char* str = malloc(sizeof(char) * 10);
+    char* result = calloc(sizeof(char), 800);
+    char* temp = calloc(sizeof(char), 100);
+    char* str = calloc(sizeof(char), 10);
 
     if (result == NULL || temp == NULL || str == NULL) {
         return -1;
@@ -39,13 +41,14 @@ int firstMode(Dictionary* d, int count)
 
     result[0] = '\0';
 
-    sprintf(temp, "Первый режим, сложность %d слов.\n", count);
+    wprintw(win, "Первый режим, сложность %d слов.\n", count);
+    sprintf(temp, "%04d-%02d-%02d\n%02d:%02d:%02d\n", aTm->tm_year + 1900, aTm->tm_mon + 1, aTm->tm_mday, aTm->tm_hour, aTm->tm_min, aTm->tm_sec);
     strcat(result, temp);
     generate_rand(&a, count, d->count - 1);
 
     for (int i = 0; i < count; i++) {
-        fprintf(stdout, "Введите перевод слова %s на русский язык:\n", d->lines[a[i]]->key);
-        fscanf(stdin, "%s", str);
+        wprintw(win, "Введите перевод слова %s на русский язык:\n", d->lines[a[i]]->key);
+        wscanw(win, "%s", str);
         node = list_lookup(d->lines[a[i]], str);
         if (node != NULL) {
             score++;
@@ -59,10 +62,11 @@ int firstMode(Dictionary* d, int count)
 
     FILE* file;
     if ((file = fopen("results.log", "a+")) == NULL) {
-        fprintf(stdout, "Не удалось открыть файл, результаты не будут записаны.");
+        wprintw(win, "Не удалось открыть файл, результаты не будут записаны.");
     }
-    fprintf(file, "%04d-%02d-%02d\n%02d:%02d:%02d\n", aTm->tm_year + 1900, aTm->tm_mon + 1, aTm->tm_mday, aTm->tm_hour, aTm->tm_min, aTm->tm_sec);
+
     fprintf(file, "%s", result);
+    wprintw(win, "%s", result);
     fclose(file);
     free(a);
     return score;
